8bdcb02c91c806f8671932cd9fbc10526e77872c
hamdani <mustapha.hamdani@gmail.com>
2024-05-02 23:52:36 +0100

    change interface and replace object by html

---
+++ src/canevasWebDev.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Fetch HTML content from external file
+  fetch("src/canevasWebDev.html")
+    .then((response) => response.text())
+    .then((html) => {
+      // Select the target element where you want to insert the HTML content
+      const targetElement = document.getElementById("content-placeholder");
+
+      // Insert the fetched HTML content into the target element
+      targetElement.insertAdjacentHTML("afterbegin", html);
+    })
+    .catch((error) => console.error("Error fetching HTML content:", error));
+});
